05f486bc39f8a8e4c56576839f06a2c535c06a72
fadilamanosi <fadilamanosi@gmail.com>
2023-11-12 21:59:13 +0100

    check csrf

--- src/axios.js
+++ src/axios.js
@@ -4,7 +4,7 @@ import Cookies from 'js-cookie';
 
 // Axios init config
 axios.defaults.withCredentials = true;
-// axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 axios.defaults.headers.common['Accept'] = 'application/json';
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 axios.defaults.baseURL = import.meta.env.VITE_API_URL;
